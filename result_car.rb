b68e8c5dfe09503457271f802e2f3c143cfd492e
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2022-11-12 19:48:47 +0000

    Update result_car.rb
    remove code

--- result_car.rb
+++ result_car.rb
@@ -7,9 +7,6 @@ class ResultCar
     @results_car = []
 
     @data.each do |car|
-      make = [0, car[:make].upcase]
-      model = [0, car[:model].upcase]
-
       if @user_data[:make] == car[:make].upcase ||
         @user_data[:model] == car[:model].upcase ||
         car[:year].between?(@user_data[:year_from], @user_data[:year_to]) &&
