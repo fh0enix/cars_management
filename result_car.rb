86629f879ef8f65a11f3b221dffde008055977a9
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2022-11-14 10:13:33 +0000

    Update result_car.rb
    little changes

--- result_car.rb
+++ result_car.rb
@@ -1,18 +1,23 @@
 class ResultCar
-  attr_reader :results_car
-
-  def initialize(data, user_data)
+  def initialize(data, user_data, results_car)
     @data = data
     @user_data = user_data
-    @results_car = []
+    @results_car = results_car
+  end
 
+  def call
     @data.each do |car|
       if @user_data[:make] == car[:make].upcase ||
-        @user_data[:model] == car[:model].upcase ||
-        car[:year].between?(@user_data[:year_from], @user_data[:year_to]) &&
-        car[:price].between?(@user_data[:price_from], @user_data[:price_to])
-        @results_car.push(car)
+        @user_data[:make] == 0
+        if @user_data[:model] == car[:model].upcase ||
+          @user_data[:model] == 0
+          if car[:year].between?(@user_data[:year_from], @user_data[:year_to]) &&
+            car[:price].between?(@user_data[:price_from], @user_data[:price_to])
+            @results_car.push(car)
+          end
+        end
       end
     end
+    @results_car
   end
 end
