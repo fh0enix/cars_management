5411c2c3310b7a83a5d96fa5125df6f814ccceba
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2022-11-18 17:06:15 +0200

    change some logic search

--- search engine/result_car.rb
+++ search engine/result_car.rb
@@ -7,21 +7,20 @@ class ResultCar
 
   def call
     @data.each do |car|
-      if @user_data[:make] == car[:make].upcase ||
-        @user_data[:make] == 0
-        if @user_data[:model] == car[:model].upcase ||
-          @user_data[:model] == 0
-          if car[:year].between?(@user_data[:year_from], @user_data[:year_to]) ||
-            car[:year] > @user_data[:year_from]
-            if car[:price].between?(@user_data[:price_from], @user_data[:price_to]) ||
-              car[:price] > @user_data[:price_from]
-              @results_car.push(car)
-            end
-          end
-        end
+      if (@user_data[:make] == car[:make].upcase || @user_data[:make] == nil) &&
+
+        (@user_data[:model] == car[:model].upcase || @user_data[:model] == nil) &&
+
+        (car[:year].between?(@user_data[:year_from],  @user_data[:year_to]) ||
+        (@user_data[:year_to].zero? && car[:year] >= @user_data[:year_from])) &&
+
+        (car[:price].between?(@user_data[:price_from], @user_data[:price_to]) ||
+        (@user_data[:price_to].zero? && car[:price] >= @user_data[:price_from]))
+
+        @results_car.push(car)
       end
     end
-    @user_data[:results_car] = @results_car.size
+    @user_data[:total_qantity] = @results_car.size
     @results_car
   end
 end
