fa5c5ef2187b5d1057dfb909ec61ee41df8fd873
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2022-11-20 17:20:51 +0200

    creating instance onece

--- search engine/search_engine.rb
+++ search engine/search_engine.rb
@@ -19,7 +19,8 @@ class SearchEngine
     Formatting.new(@data, @user_data).call
     ResultCar.new(@data, @user_data, @results_car).call
     Sorting.new(@user_data, @results_car).call
-    Printer.new(@results_car, @user_data, @stat).stat
-    Printer.new(@results_car, @user_data, @stat).results
+    printer = Printer.new(@results_car, @user_data, @stat)
+    printer.stat
+    printer.results
   end
 end
