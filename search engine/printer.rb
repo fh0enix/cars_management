78110d3636af456a635b85195417ea9bdb10efcc
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2022-11-24 10:42:15 +0200

    Update printer.rb
    add prettify-output-to-console

--- search engine/printer.rb
+++ search engine/printer.rb
@@ -1,5 +1,7 @@
 require_relative 'statistic'
 require 'i18n'
+require 'terminal-table'
+require 'colorize'
 
 class Printer
   def initialize(results_car, user_data, stat)
@@ -8,21 +10,20 @@ class Printer
     @stat = stat
   end
 
-  def results
-    puts '----------------------------------'
-    puts I18n.t(:results)
-    @results_car.each do |car|
-      car.each do |attribute_name, attribute_value|
-        puts "#{attribute_name}: #{attribute_value}"
-      end
-      puts '----------------------------------'
+  def call
+    stat = Terminal::Table.new do |t|
+      t.style = { alignment: :center, width: 60, border_x: '=', border_i: 'x' }
+      t.title = I18n.t(:statistic).colorize(:green)
+      t << [I18n.t(:total_quantity), @user_data[:total_qantity]]
+      t << [I18n.t(:requests_quantity), Statistic.new(@user_data, @results_car, @stat).call]
     end
-  end
 
-  def stat
-    puts '----------------------------------'
-    puts I18n.t(:statistic)
-    puts "#{I18n.t(:total_quantity)} #{@user_data[:total_qantity]}"
-    puts "#{I18n.t(:requests_quantity)} #{Statistic.new(@user_data, @results_car, @stat).call}"
+    table = Terminal::Table.new do |t|
+      t.style = { border_x: '=', border_i: 'x' }
+      t.title = I18n.t(:results).colorize(:green)
+      @results_car.each { |car| car.each { |row| t << row } }
+    end
+    puts stat
+    puts table
   end
 end
