50cdb5b5d7cacaa3d2f91a277d02556816b2c9d5
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2022-11-26 14:24:54 +0200

    SORT_BY_PRICE_OPTION
    SORT_ASC_OPTION

--- search engine/sorting.rb
+++ search engine/sorting.rb
@@ -1,17 +1,18 @@
 class Sorting
-  SORT_ANSWER_PRICE_OR_ASC = 1
+  SORT_BY_PRICE_OPTION = 1
+  SORT_ASC_OPTION = 1
   def initialize(user_data, results_car)
     @user_data = user_data
     @results_car = results_car
   end
 
   def call
-    if @user_data[:sort_option] == SORT_ANSWER_PRICE_OR_ASC
+    if @user_data[:sort_option] == SORT_BY_PRICE_OPTION
       @results_car.sort_by! { |k| k[:price] }
     else
       @results_car.sort_by! { |k| k[:date_added] }
     end
 
-    @results_car.reverse! if @user_data[:sort_direction] == SORT_ANSWER_PRICE_OR_ASC
+    @results_car.reverse! if @user_data[:sort_direction] == SORT_ASC_OPTION
   end
 end
