266ad8673fe29083d1e221145baa8fd1b89c53cf
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2022-11-26 14:20:42 +0200

    LITTLE CHANGE

--- search engine/sorting.rb
+++ search engine/sorting.rb
@@ -1,17 +1,17 @@
 class Sorting
-  SORT_ANSWER = 1
+  SORT_ANSWER_PRICE_OR_ASC = 1
   def initialize(user_data, results_car)
     @user_data = user_data
     @results_car = results_car
   end
 
   def call
-    if @user_data[:sort_option] == SORT_ANSWER
+    if @user_data[:sort_option] == SORT_ANSWER_PRICE_OR_ASC
       @results_car.sort_by! { |k| k[:price] }
     else
       @results_car.sort_by! { |k| k[:date_added] }
     end
 
-    @results_car.reverse! if @user_data[:sort_direction] == SORT_ANSWER
+    @results_car.reverse! if @user_data[:sort_direction] == SORT_ANSWER_PRICE_OR_ASC
   end
 end
