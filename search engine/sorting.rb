c904d0703ef115b1d9f5248d9b6fbbc9e09687ed
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2022-11-26 14:18:14 +0200

    add sort answer constant

--- search engine/sorting.rb
+++ search engine/sorting.rb
@@ -1,16 +1,17 @@
 class Sorting
+  SORT_ANSWER = 1
   def initialize(user_data, results_car)
     @user_data = user_data
     @results_car = results_car
   end
 
   def call
-    if @user_data[:sort_option] == 1
-      @results_car.sort_by!{ |k| k[:price]}
+    if @user_data[:sort_option] == SORT_ANSWER
+      @results_car.sort_by! { |k| k[:price] }
     else
-      @results_car.sort_by!{ |k| k[:date_added]}
+      @results_car.sort_by! { |k| k[:date_added] }
     end
 
-    @results_car.reverse! if @user_data[:sort_direction] == 1
+    @results_car.reverse! if @user_data[:sort_direction] == SORT_ANSWER
   end
 end
