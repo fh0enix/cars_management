0a691722436c056da64ccddc0e15e01c59333d05
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2022-12-20 15:00:37 +0200

    corrected random year

--- admin/generate_car_db.rb
+++ admin/generate_car_db.rb
@@ -45,7 +45,7 @@ class GenerateCarDb
   end
 
   def random_date
-    "#{rand(1..28)}/#{rand(1..12)}/#{rand(1999..2022)}"
+    "#{rand(1..28)}/#{rand(1..12)}/#{rand(2021..2022)}"
   end
 
   def random_hex
